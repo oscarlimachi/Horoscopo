45795da572897650dec1101404598c21d09fb25e
Oscar Limachi Tapia <oscarlimachitapia@gmail.com>
2025-04-09 14:32:01 +0200

    empieza la navegacion

--- app/src/main/java/com/example/horoscopo/activities/DetailActivity.kt
+++ app/src/main/java/com/example/horoscopo/activities/DetailActivity.kt
@@ -1,14 +1,22 @@
 package com.example.horoscopo.activities
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.horoscopo.R
+import com.example.horoscopo.data.HoroscopeProvider
 
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var nameTextView: TextView
+    lateinit var dateTextView: TextView
+    lateinit var iconImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,7 +26,16 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val id = intent.getStringExtra("HOROSCOPE_ID")
-        Toast.makeText(this,id, Toast.LENGTH_SHORT).show()
+        nameTextView = findViewById(R.id.nameTextView)
+        dateTextView = findViewById(R.id.dateTextView)
+        iconImageView = findViewById(R.id.iconImageView)
+
+        val id=intent.getStringExtra("HOROSCOPE_ID")!!
+        val horoscope = HoroscopeProvider.getById(id)!!
+        Toast.makeText(this,getString(horoscope.name), Toast.LENGTH_SHORT).show()
+        nameTextView.setText(horoscope.name)
+        dateTextView.setText(horoscope.dates)
+        iconImageView.setImageResource(horoscope.icon)
+
     }
 }
